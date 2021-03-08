9d0f5a4ce338f8a6fad010cd37fb062e27e181f9
JuiceFresh <80225271+JuiceFresh@users.noreply.github.com>
2021-03-07 20:38:11 -0500

    Add files via upload

---
+++ js/funciones.js
@@ -0,0 +1,84 @@
+function Functionmenu(){
+    var x = document.getElementById("myTopnav");
+    if (x.className === "nav-menu"){
+        x.className += " responsive";
+    } else {
+        x.className = "nav-menu";
+    }
+}
+function in1(){
+    var frame = $('#frame');
+    var url = 'https://es.wikipedia.org/wiki/Helado_de_fresa';
+    frame.attr('src',url).show();
+}
+function in2(){
+    var frame = $('#frame');
+    var url = 'https://en.wikipedia.org/wiki/Chocolate_ice_cream';
+    frame.attr('src',url).show();
+}
+function in3(){
+    var frame = $('#frame');
+    var url = 'https://es.wikipedia.org/wiki/Helado_de_vainilla';
+    frame.attr('src',url).show();
+}
+function in4(){
+    var frame = $('#frame');
+    var url = 'https://es.wikipedia.org/wiki/Dulce_de_leche';
+    frame.attr('src',url).show();
+}
+function in5(){
+    var frame = $('#frame');
+    var url = 'https://www.conaprole.uy/producto/producto-helados-impagua-helado-palito-de-agua-frutilla/';
+    frame.attr('src',url).show();
+}
+function in6(){
+    var frame = $('#frame');
+    var url = 'https://www.dulcesentimiento.com/helado-de-chicle/';
+    frame.attr('src',url).show();
+}
+function in7(){
+    var frame = $('#frame');
+    var url = 'https://www.fitatu.com/catalog/es/helado-de-melocoton--54605050';
+    frame.attr('src',url).show();
+}
+function in8(){
+    var frame = $('#frame');
+    var url = 'https://www.contarproteinas.com/recetas_resultado/ver/Copa-de-helado-capuccino-7739';
+    frame.attr('src',url).show();
+}
+function in9(){
+    var frame = $('#frame');
+    var url = 'https://www.fatsecret.cl/calor%C3%ADas-nutrici%C3%B3n/san-francisco/helado-de-brownie/1-2-taza?frc=True#:~:text=Resumen%20Nutricional%3A&text=Hay%20175%20calor%C3%ADas%20en%201,%25%20carbh%2C%2011%25%20prot.';
+    frame.attr('src',url).show();
+}
+function in10(){
+    var frame = $('#frame');
+    var url = 'https://www.vegaffinity.com/comunidad/alimento/helado-con-galletas-oreo-beneficios-informacion-nutricional--f766';
+    frame.attr('src',url).show();
+}
+function in11(){
+    var frame = $('#frame');
+    var url = 'https://enrilemoine.com/2019/06/05/helado-de-mora-receta/';
+    frame.attr('src',url).show();
+}
+function in12(){
+    var frame = $('#frame');
+    var url = 'https://www.mujerdeelite.com/guia_de_alimentos/5605/coco-helado-helado-de-coco-la-ibense-bornay';
+    frame.attr('src',url).show();
+}
+function in13(){
+    var frame = $('#frame');
+    var url = 'https://www.vegaffinity.com/comunidad/alimento/helado-con-nueces-pecanas-tofutti-better-pecan-beneficios-informacion-nutricional--f778';
+    frame.attr('src',url).show();
+}
+function in14(){
+    var frame = $('#frame');
+    var url = 'https://www.fatsecret.cl/calor%C3%ADas-nutrici%C3%B3n/san-francisco/helado-frambuesa/1-2-taza';
+    frame.attr('src',url).show();
+}
+function in15(){
+    var frame = $('#frame');
+    var url = 'https://es.wikipedia.org/wiki/Helado_de_pistacho';
+    frame.attr('src',url).show();
+}
+
